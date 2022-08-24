fd89644a54884af4f0dc8587d7e9d36b445f35ea
krg-nandu <krg.lakshminarasimhan@gmail.com>
2022-08-24 16:34:09 -0400

    training loop

--- datasets.py
+++ datasets.py
@@ -3,17 +3,29 @@ import pandas as pd
 import os
 from torch.utils.data import Dataset
 from copy import deepcopy
+import matplotlib.pyplot as plt
+import torch
 
 class FordADataset(Dataset):
     def __init__(self):
         self.root_url = "https://raw.githubusercontent.com/hfawaz/cd-diagram/master/FordA/"
         x, y = self.read_data(self.root_url + "FordA_TRAIN.tsv")
-        self.train_data = deepcopy(x)
+        self.train_data = np.expand_dims(deepcopy(x), -1)
         self.train_labels = deepcopy(y)
+        self.train_labels[self.train_labels == -1] = 0
 
         x, y = self.read_data(self.root_url + "FordA_TEST.tsv")
-        self.test_data = deepcopy(x)
+        self.test_data = np.expand_dims(deepcopy(x), -1)
         self.test_labels = deepcopy(y)
+        self.test_labels[self.test_labels == -1] = 0
+       
+        self.num_classes = len(np.unique(self.train_labels))
+
+    def __len__(self):
+        return len(self.train_labels)
+
+    def __getitem__(self, idx):
+        return torch.Tensor(self.train_data[idx]).float(), torch.Tensor([self.train_labels[idx]]).long()
 
     def read_data(self, filename):
         data = np.loadtxt(filename, delimiter="\t")
@@ -21,5 +33,37 @@ class FordADataset(Dataset):
         x = data[:, 1:]
         return x, y.astype(int)
 
-    def visualize_samples(self):
-        import ipdb; ipdb.set_trace()
+    ''' Randomly pick n samples from each class to visualize
+    This is a binary classification problem
+    '''
+    def visualize_samples(self, n, mode):
+        fig = plt.figure(figsize=(12,4))
+        ax1 = fig.add_subplot(121)
+        ax2 = fig.add_subplot(122)
+        
+        if mode == 'train':
+            X_data, X_labels = self.train_data, self.train_labels
+        elif mode == 'test':
+            X_data, X_labels = self.test_data, self.test_labels
+
+        posIdx = np.where(X_labels == 1)[0]
+        idx = np.random.randint(posIdx.shape[0], size=(n,))
+        posSamples = X_data[idx, :, 0].T
+
+        negIdx = np.where(X_labels == 0)[0]
+        idx = np.random.randint(negIdx.shape[0], size=(n,))
+        negSamples = X_data[idx, :, 0].T
+
+        ax1.plot(posSamples)
+        ax1.set_xlabel('Time', fontsize=16, fontweight='bold')
+        ax1.spines['top'].set_visible(False)
+        ax1.spines['right'].set_visible(False)
+        ax1.set_title('Positive Samples', fontweight='bold', fontsize=16)
+
+        ax2.plot(negSamples)
+        ax2.set_xlabel('Time', fontsize=16, fontweight='bold')
+        ax2.spines['top'].set_visible(False)
+        ax2.spines['right'].set_visible(False)
+        ax2.set_title('Negative Samples', fontweight='bold', fontsize=16)
+
+        plt.show()
--- gru_classification_scratch.py
+++ gru_classification_scratch.py
@@ -1,6 +1,8 @@
 import torch
+import torch.nn as nn
 import matplotlib.pyplot as plt
 import numpy as np
+from datasets import FordADataset
 
 class GRU(torch.nn.Module):
     def __init__(self, num_inputs, num_hiddens, sigma=0.01):
@@ -47,3 +49,55 @@ class GRU(torch.nn.Module):
             outputs.append(H)
         return outputs, (H, )
 
+def train_model(model, dataset, params):
+
+    # create the data generator to iterate over mini batches
+    trainDataGenerator = torch.utils.data.DataLoader(dataset, **params['train_params'])
+
+    criterion = torch.nn.MSELoss()    
+    optimizer = torch.optim.Adam(model.parameters(), lr=params['init_lr'])
+
+    for epoch in range(params['num_epochs']):
+
+        for data, label in trainDataGenerator:
+            optimizer.zero_grad()
+
+            # forward pass
+            output, latents = model(data)
+
+            # compute the loss
+            loss = criterion()
+
+            # backpropagate through time!
+            loss.backward()
+
+            # update model parameters
+            optimizer.step()
+
+            if epoch%5 == 0:
+                print('loss: {}'.format(loss.item()))
+
+
+if __name__ == '__main__':
+    fordDataset = FordADataset()
+    #fordDataset.visualize_samples(n = 25, mode = 'train')
+
+    params = {
+        'n_inputs': 1,
+        'n_hidden': 32,
+        'num_epochs': 100,
+        'init_lr': 1e-4,
+        'n_classes': 2,
+
+        'train_params': {
+                    'batch_size': 1024,
+                    'shuffle': True,
+                    'num_workers': 0
+                }
+    }
+
+    model = GRU(params['n_inputs'], params['n_hidden'])
+    # if you want to port the mode to GPU
+    # model = model.to('cuda:0')
+
+    train_model(model, fordDataset, params)
